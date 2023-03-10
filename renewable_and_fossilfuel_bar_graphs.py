dcac59d6b53456f8a7ba5aea7cd943c3cc1d16d0
vvishnus <98347865+vvishnus@users.noreply.github.com>
2023-03-10 11:38:00 -0800

    Second Commit

--- renewable_and_fossilfuel_bar_graphs.py
+++ renewable_and_fossilfuel_bar_graphs.py
@@ -14,7 +14,6 @@ energy_df = raw[energy_consumption]
 year_range = energy_df["year"].isin(range(2000,2020))
 energy_df = energy_df[year_range]
 
-#print(energy_df.columns)
 # Create a copy of the energy_df dataframe to avoid the warning
 energy_df_copy = energy_df.copy()
 energy_df_copy.rename({"iso_code":"ISO_A3"}, axis=1, inplace=True)
@@ -25,10 +24,13 @@ bar1.reset_index(level=0, inplace=True)
 colors2 = ['lightgrey', 'steelblue', 'lightskyblue', 'peru', 'khaki']
 colors1 = ['steelblue', 'lightskyblue', 'peru', 'khaki']
 
+# Plot the stacked bar graph for Global renewable energy consumption
 bar1.plot(x='year', ylabel='Terrawatt Hours of Energy', kind='bar', stacked=True, figsize=(20,8), title='Global Renewable Energy Consumption between 2000-2019', color = colors1)
 plt.show()
 
 bar2 = energy_df_copy.groupby('year')[renewable_consumption2].sum()
 bar2.reset_index(level=0, inplace=True)
+
+# Plot the stacked bar graph for Global renewable + Fossil fuel energy consumption
 bar2.plot(x='year', ylabel='Terrawatt Hours of Energy', kind='bar', stacked=True, figsize=(20,8), title='Global Renewable + fossil fuel Energy Consumption between 2000-2019', color = colors2)
 plt.show()
